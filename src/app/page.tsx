97b477937e0f9c5c4d2dc76e900ff2f09f58d35d
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-22 20:03:47 -0300

    feat: add theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default async function Home() {
   return (
     <main className="container px-1 py-10 md:p-10">
       <h1>Loja</h1>
+      <ModeToggle />
     </main>
   );
 }
