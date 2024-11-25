06197ad3ab0b41fd917ee88fa17a7b44c0f1341e
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-25 07:50:40 -0300

    feat: add tabs trigger

--- src/components/products/tab.tsx
+++ src/components/products/tab.tsx
@@ -1,19 +1,48 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getProducts } from "@/services/product";
+import { ProductProps } from "@/types/product";
+
+type TabProps = {
+  title: string;
+  value: string;
+  products: ProductProps[];
+};
 
 export async function ProductsTab() {
   const products = await getProducts();
   console.log(products);
 
+  const tabs: TabProps[] = [
+    {
+      title: "sushi",
+      value: "sushi",
+      products: [],
+    },
+    {
+      title: "Temaki",
+      value: "temaki",
+      products: [],
+    },
+    {
+      title: "Combinados",
+      value: "pack",
+      products: [],
+    },
+    {
+      title: "Bebidas",
+      value: "beverage",
+      products: [],
+    },
+  ];
+
   return (
     <Tabs defaultValue="tab1">
       <TabsList className="flex mb-6">
-        <TabsTrigger value="tab1" className="flex-1">
-          Tab 1
-        </TabsTrigger>
-        <TabsTrigger value="tab2" className="flex-1">
-          Tab 2
-        </TabsTrigger>
+        {tabs.map(({ title, value }) => (
+          <TabsTrigger key={title} value={value} className="flex-1">
+            {title}
+          </TabsTrigger>
+        ))}
       </TabsList>
 
       <TabsContent value="tab1">Conteúdo tab1</TabsContent>
