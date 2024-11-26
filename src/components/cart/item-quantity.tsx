5522d65c840b5bd073e794edd67cf9bfa6195d6e
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-26 09:00:02 -0300

    feat: add buttons to ItemQuantity component

--- src/components/cart/item-quantity.tsx
+++ src/components/cart/item-quantity.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { MinusIcon, PlusIcon } from "lucide-react";
+
 export function CartItemQuantity({ quantity }: { quantity: number }) {
   return (
-    <div>
-      <p>{quantity}</p>
+    <div className="flex items-center gap-2">
+      <Button variant={"outline"} size={"icon"} className="size-4">
+        <PlusIcon size={10} />
+      </Button>
+
+      <p className="text-xs">{quantity}</p>
+
+      <Button variant={"outline"} size={"icon"} className="size-4">
+        <MinusIcon size={10} />
+      </Button>
     </div>
   );
 }
