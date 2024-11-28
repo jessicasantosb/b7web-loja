b00b50b07d7a628265f467cbf4c319ad0f900125
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-28 07:54:32 -0300

    feat: show checkout title and progress bar

--- src/components/checkout/dialog.tsx
+++ src/components/checkout/dialog.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   Dialog,
   DialogContent,
@@ -6,20 +8,42 @@ import {
 } from "@/components/ui/dialog";
 import { Progress } from "@/components/ui/progress";
 
+type StepsProps = "user" | "address" | "finish";
+
 type ChekoutProps = {
   open: boolean;
   onOpenChange: (open: boolean) => void;
 };
 
 export function CheckoutDialog({ open, onOpenChange }: ChekoutProps) {
+  const [step, setStep] = useState<StepsProps>("user");
+
+  let progressBar = 0;
+  let stepTitle = "";
+
+  switch (step) {
+    case "user":
+      progressBar = 30;
+      stepTitle = "Dados Pessoais";
+      break;
+    case "address":
+      progressBar = 70;
+      stepTitle = "Endereço de entrega";
+      break;
+    case "finish":
+      progressBar = 100;
+      stepTitle = "Envio para o Whatsapp";
+      break;
+  }
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent>
+      <DialogContent aria-describedby={undefined}>
         <DialogHeader>
-          <DialogTitle>...</DialogTitle>
+          <DialogTitle>{stepTitle}</DialogTitle>
         </DialogHeader>
 
-        <Progress value={66} />
+        <Progress value={progressBar} />
 
         <div className="flex flex-col gap-3"></div>
       </DialogContent>
