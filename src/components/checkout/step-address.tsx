6421ed11913e9e403c4e3c062a73571fdcbc2f5f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-28 10:53:56 -0300

    feat: add select to state field

--- src/components/checkout/step-address.tsx
+++ src/components/checkout/step-address.tsx
@@ -15,6 +15,13 @@ import { stepAddressSchema } from "@/schemas/step-address";
 import { useCheckoutStore } from "@/stores/checkout-store";
 import { SetStepProps } from "@/types/checkout-steps";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 export function StepAddress({ setStep }: SetStepProps) {
   const { address, setAddress } = useCheckoutStore((state) => state);
@@ -102,6 +109,7 @@ export function StepAddress({ setStep }: SetStepProps) {
               </FormItem>
             )}
           />
+
           <FormField
             name="state"
             control={form.control}
@@ -109,7 +117,19 @@ export function StepAddress({ setStep }: SetStepProps) {
               <FormItem>
                 <FormLabel>Estado</FormLabel>
                 <FormControl>
-                  <Input {...field} />
+                  <Select
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="selecione o estado" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="mg">Minas Gerais</SelectItem>
+                      <SelectItem value="ce">Ceará</SelectItem>
+                      <SelectItem value="pr">Paraná</SelectItem>
+                    </SelectContent>
+                  </Select>
                 </FormControl>
                 <FormMessage />
               </FormItem>
